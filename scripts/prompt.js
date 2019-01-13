ea13a9d4bfc14c5378b7e539b25260b9d2010b94
dustinsnoap <afrobacon@gmail.com>
2019-01-13 01:42:59 -0500

    Fixed issue where user couldn't enter multiple spaces Change prompt from span to pre to fix issue above Changed cursor from :after to it's own span to fix alignment issues Added 1 space margin-left to container

--- scripts/prompt.js
+++ scripts/prompt.js
@@ -22,10 +22,12 @@ function addText(e) {
     console.log(e.keyCode);
     //backspace (remove last char from string)
     if(e.keyCode == 8) el.textContent = el.textContent.slice(0, -1);
+    //space
+    if(e.keyCode == 32) el.textContent = el.textContent += e.key;
     //a-zA-Z
     if(e.keyCode >= 48 && e.keyCode <= 90) el.textContent += e.key;
     //0-9
     if(e.keyCode >= 96 && e.keyCode <= 111) el.textContent += e.key;
     //punctuation and space
-    if(e.keyCode >= 186 && e.keyCode <= 222 || e.keyCode == 32) el.textContent += e.key;
+    if(e.keyCode >= 186 && e.keyCode <= 222) el.textContent += e.key;
 }
