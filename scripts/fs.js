72fd891c21e08bd1a8f5f0297adeb0801483f744
dustinsnoap <afrobacon@gmail.com>
2019-01-14 01:47:30 -0500

    Added code for keyword "ls"

---
+++ scripts/fs.js
@@ -0,0 +1,7 @@
+function getDir() {
+    let dir = ["~"];
+    dir.push("education.txt");
+    dir.push("work-history.txt");
+    dir.push(["one", "two", "three"]);
+    return dir;
+}
--- scripts/functioncalls.js
+++ scripts/functioncalls.js
@@ -4,13 +4,16 @@ function callStack(keyword) {
     //declare this elsewhere so it only loads once
     let callStack = []; //[keyword, function call]
     callStack.push(["clear", fnClear]);
+    callStack.push(["ls", fnLS]);
     
     let result = callStack.filter(function(call) {
         //there's an app for that
-        if(call[0] === keyword) call[1]();
-        //nope
-        else fnNotFound(keyword);
+        if(call[0] === keyword) {
+            call[1]();
+            return call;
+        }
     });
+    if(result.length == 0) fnNotFound(keyword);
 }
 //function!found
 function fnNotFound(keyword) {
@@ -27,4 +30,25 @@ function fnNotFound(keyword) {
 function fnClear() {
     let terminal = document.getElementById("terminal");
     while(terminal.firstChild) terminal.removeChild(terminal.firstChild);
+}
+//ls
+function fnLS() {
+    let dir = getDir();
+    let terminal = document.getElementById("terminal");
+    let line = document.createElement("div");
+    line.classList.add("line");
+    dir.forEach(function(el, i) {
+        if(i > 0) {
+            let file = document.createElement("span");
+            file.classList.add("color-operator", "fs");
+            if(Array.isArray(el)) {
+                file.classList.add("dir");
+                file.textContent = el[0];
+            } else {
+                file.textContent = el;
+            }
+            line.appendChild(file);
+        }
+    });
+    terminal.appendChild(line);
 }
